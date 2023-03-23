36f68089c0986eb0e20eaced63728dd82dc3e1b6
lifecycles1 <raichev89@gmail.com>
2023-03-23 21:34:07 +0000

    update

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -2,6 +2,7 @@ global using BlazorEcommerce.Shared;
 global using BlazorEcommerce.Client.Services.ProductService;
 global using BlazorEcommerce.Client.Services.CategoryService;
 global using BlazorEcommerce.Client.Services.CartService;
+global using BlazorEcommerce.Client.Services.AuthService;
 using BlazorEcommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -16,5 +17,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 await builder.Build().RunAsync();
---
+++ BlazorEcommerce/Server/Services/AuthService/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorEcommerce.Server.Services.AuthService
+{
+    public interface IAuthService
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<bool> UserExists(string email);
+        Task<ServiceResponse<string>> Login(string email, string password);
+    }
+}
